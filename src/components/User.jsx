9ed317a447b77523b5477b43268c4a7a9da321ff
Mustafa Alroomi <mustafa.alroomi@appfoundry.be>
2019-01-06 00:32:08 +0100

    routing

---
+++ src/components/User.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+
+const User = () => (
+  <div>User: 1</div>
+)
+
+export default User
---
+++ src/containers/UsersContainer.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+
+class UsersContainer extends Component {
+  render(){
+    return (
+      <div>
+        UsersContainer
+      </div>
+    )
+  }
+}
+
+export default UsersContainer
