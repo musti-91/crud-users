4777fd289e4104c271f913f3848ffb68f37541bb
Mustafa Alroomi <mustafa.alroomi@appfoundry.be>
2019-01-06 01:23:18 +0100

    redux

--- src/redux/UserRedux.js
+++ src/redux/UserRedux.js
@@ -1,43 +1,47 @@
-import {} from 'seamless-immutable'
+import Immutable from 'seamless-immutable'
 import { createActions, createReducer } from 'reduxsauce'
 
-//types
-const { Types, Creators } = createActions({
+// State => users state
+const INITIAL_STATE = Immutable({
+  fetching: false,
+  users: [],
+  fetchingError: null
+})
+
+// Actions
+const { Types, Creators } = createActions({
   fetchUsersStart: null,
   fetchUsersSuccess: ['users'],
   fetchUsersFailure: ['error'],
-  resetFetchError: null,
+  resetFetchError: null
 })
 
 export const UserTypes = Types
 export default Creators
-//state
-const initial_state = {
-  fetching: false,
-  users: [],
-  fetchingError: null,
-}
-// actions
 
-export const fetchUsersStart= (state) => state.merge({
-  fetching: true,
-  users: [],
-  fetchingError:null
-})
-export const fetchUsersSuccess= (state, { users }) => state.merge({
-  fetching: false,
-  users,
-  fetchingError: null,
-})
-export const fetchUsersFailure= (state, { error }) => state.merge({
-  fetching: false,
-  fetchingError: error
-})
-export const resetFetchError = (state) => state.merge({ fetchingError: null })
+export const fetchUsersStart = state =>
+  state.merge({
+    fetching: true,
+    users: [],
+    fetchingError: null
+  })
+export const fetchUsersSuccess = (state, { users }) =>
+  state.merge({
+    fetching: false,
+    users,
+    fetchingError: null
+  })
+export const fetchUsersFailure = (state, { error }) =>
+  state.merge({
+    fetching: false,
+    fetchingError: error
+  })
+export const resetFetchError = state => state.merge({ fetchingError: null })
 
-export const reducer= createReducer(initial_state, {
+// Types
+export const reducer = createReducer(INITIAL_STATE, {
   [Types.FETCH_USERS_START]: fetchUsersStart,
   [Types.FETCH_USERS_SUCCESS]: fetchUsersSuccess,
   [Types.FETCH_USERS_FAILURE]: fetchUsersFailure,
-  [Types.RESET_FETCH_ERROR]: resetFetchError,
+  [Types.RESET_FETCH_ERROR]: resetFetchError
 })
